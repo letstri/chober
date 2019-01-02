d7decd256b0a1ece5ed1429bf97a111532dbe421
Valery Strelets <brooons@yandex.ru>
2019-01-02 21:59:11 +0200

    optimization

--- webpack.config.js
+++ webpack.config.js
@@ -8,21 +8,16 @@ module.exports = env => ({
   mode: env,
   devtool: 'none',
   output: {
-    filename: env === 'development' ? 'chober.js' : 'chober.min.js',
+    filename: env === 'production' ? 'chober.min.js' : 'chober.js',
     path: path.resolve(__dirname, 'dist'),
     library: LIBRARY_SHORT_NAME,
+    libraryExport: 'default',
     libraryTarget: 'umd',
     umdNamedDefine: true,
     globalObject: 'typeof self === \'undefined\' ? this : self',
   },
   optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          keep_fnames: true,
-        },
-      }),
-    ],
+    minimizer: [new UglifyJsPlugin()],
   },
   module: {
     rules: [
