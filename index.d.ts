c9833b75992574ae0de8b206371b9fcb5bbfa203
BrooonS <brooons@yandex.ru>
2019-03-28 22:55:25 +0200

    add  index d ts

---
+++ index.d.ts
@@ -0,0 +1,51 @@
+export function clone(item: any): any;
+
+export function flattenDeep(array: Array<any>): Array<any>;
+
+export function formatNumber(number: number | string, symbol: string): string;
+
+export function getCookie(key: string): string;
+
+export function getOffset(selector: string): object;
+
+export function getQuery(arrayFields: Array<string>): object;
+
+export function getScrollbarWidth(): number;
+
+export function getType(item: any): string;
+
+export function isArray(item: any): boolean;
+
+export function isBoolean(item: any): boolean;
+
+export function isEmpty(item: any): boolean;
+
+export function isEqual(firstItem: any, secondItem: any): boolean;
+
+export function isFunction(item: any): boolean;
+
+export function isNil(item: any): boolean;
+
+export function isNumber(item: any): boolean;
+
+export function isObject(item: any): boolean;
+
+export function isString(item: any): boolean;
+
+export function isUndefined(item: any): boolean;
+
+export function removeCookie(key: string): void;
+
+export function scrollTo(scrollTo: HTMLElement): void;
+
+export function setCookie(key: string, value: string, expireIn: boolean): void;
+
+interface setQueryConfig {
+  isSaveOld: boolean,
+  isSaveHash: boolean,
+  isSaveEmptyFields: boolean,
+}
+
+export function setQuery(query: object, config: setQueryConfig): void;
+
+export function uniq(array: Array<any>): Array<any>;
